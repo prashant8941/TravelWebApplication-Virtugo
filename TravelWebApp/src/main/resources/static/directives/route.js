1105c8f08fae4e7cabaa8c727ad9c584418a284b
Prashant Sharma <ps65854321@gmail.com>
2026-04-15 23:57:21 +0530

    Initial commit

---
+++ TravelWebApp/src/main/resources/static/directives/route.js
@@ -0,0 +1,42 @@
+var travelApp = angular.module("TravelApp",["ngRoute","ui.bootstrap","ngMap"]);
+alert("route");
+travelApp.config(function($routeProvider){
+	$routeProvider.
+	when("/home",{
+		templateUrl:"templates/home.html",
+		controller:"homeController"
+	})
+	.when("/dashboard",{
+		templateUrl:"templates/dashboard.html"
+	})
+	.when("/index",{
+		templateUrl:"templates/home.html",
+		controller:"baseController"
+	})
+	.when("/login",{
+		templateUrl:"templates/login.html",
+		controller:"loginController"
+	})
+	.otherwise({
+		redirectTo:"/home"
+	});
+});
+
+
+travelApp.controller("baseController",function($scope,$location,$route){
+	alert("&&&&&&&&&&&&");
+	var template = "";
+	$scope.login = function(){
+		alert("openpopup");
+		/*$scope.loginForm.$setPristine();
+		 $scope.registerForm.$setPristine();*/
+		 alert($location.path());
+		 if($location.path() == "/login"){
+			 $route.reload();
+		 }else{
+		$location.path('/login');}
+		 
+		 
+	}
+	
+});
